6d3c2a40a0c983eec2c55c0643a5c30a94e37025
masterkudi <cudini.andrea@gmail.com>
2024-02-12 00:26:43 +0100

    task completed

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,37 @@
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel();
+    $(".owl-carousel").owlCarousel(
+        {
+            items: 1,
+            loop: true,
+            margin: 5,
+            autoplay: true,
+            autoplayTimeout: 2000,
+            autoplayHoverPause: true,
+            dots: false,
+            responsive: {
+                0: {
+                    items: 1
+                },
+            },
+            autoplaySpeed: 2000,
+        }
+    );
+    $('.play').on('click',function(){
+        owl.trigger('play.owl.autoplay',[1000])
+    })
+    $('.stop').on('click',function(){
+        owl.trigger('stop.owl.autoplay')
+    })
+});
+
+document.getElementById("clickButton").addEventListener("click", function() {
+    let currentDate = new Date();
+    let dateString = currentDate.toLocaleTimeString() + " on " + currentDate.toLocaleDateString();
+    alert("When I click the pop-up at " + dateString);
+});
+
+document.getElementById("clickButton2").addEventListener("click", function() {
+    let currentDate = new Date();
+    let dateString = currentDate.toLocaleTimeString() + " on " + currentDate.toLocaleDateString();
+    alert("When I click the pop-up at " + dateString);
 });
